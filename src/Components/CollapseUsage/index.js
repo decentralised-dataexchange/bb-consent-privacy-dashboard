e7a15a37ec38dd116b3c399cdbda9ab33499d51b
MARZOOQUE <mmarzooque@gmail.com>
2024-10-03 18:32:34 +0530

    Feat: #23 panel issues

--- src/Components/CollapseUsage/index.js
+++ src/Components/CollapseUsage/index.js
@@ -7,6 +7,9 @@ import { PanelHeaderWithNamespace } from "./PanelHeader";
 import { SubCollapse } from "./SubCollapse";
 import { store } from "Provider/store";
 import { withNamespaces } from "react-i18next";
+import { ModalComponentWithNamespace } from "../modals/modal";
+import { DataSourcesDetailsWithNamespace } from "../modals/detailsContainer/dataSourcesDetails";
+import { DataAgreementDetailsWithNamespace } from "../modals/detailsContainer/dataAgreementDetails";
 
 const Panel = Collapse.Panel;
 
@@ -21,7 +24,9 @@ class CollapseUsage extends Component {
     this.state = {
       openKey: [],
       scrolled: false,
-      activeKeys: [],
+      openDataSources: false,
+      openDataAgreement: false,
+      selectedDataAgreement: null,
     };
 
     this.delta = this.delta.bind(this);
@@ -50,8 +55,24 @@ class CollapseUsage extends Component {
     }
   };
 
-  onCollapseChange = (activeKeys) => {
-    this.setState({ activeKeys });
+  setOpenViewDataSources = (open) => {
+    this.setState({ openDataSources: open });
+  };
+
+  openDataSourcesModal = (dataAgreement, e) => {
+    e.stopPropagation();
+    this.setState({
+      openDataSources: true,
+      selectedDataAgreement: dataAgreement,
+    });
+  };
+
+  openDataAgreementModal = (dataAgreement, e) => {
+    e.stopPropagation();
+    this.setState({
+      openDataAgreement: true,
+      selectedDataAgreement: dataAgreement,
+    });
   };
 
   render() {
@@ -59,7 +80,11 @@ class CollapseUsage extends Component {
 
     const { t } = this.props;
     let openKey = "";
-    const { activeKeys } = this.state;
+    const {
+      openDataSources,
+      openDataAgreement,
+      selectedDataAgreement,
+    } = this.state;
 
     if (isFetching) {
       return (
@@ -74,104 +99,167 @@ class CollapseUsage extends Component {
       return acc;
     }, {});
 
-    // Todo : Need to fix the toggle, purpose highlight.
+    return (
+      <>
+        {!data ? null : (
+          <Collapse
+            className="collapse-usage"
+            bordered={false}
+            defaultActiveKey={openKey.toString()}
+            expandIcon={({ isActive }) => (
+              <Icon
+                style={{ position: "absolute", top: "22px" }}
+                type="caret-right"
+                rotate={isActive ? 90 : 0}
+              />
+            )}
+          >
+            {!data
+              ? null
+              : data.map((dataAgreement, i) => {
+                  const consented =
+                    dataAgreement["consentRecords"] === null
+                      ? 0
+                      : dataAgreement["consentRecords"]["optIn"]
+                      ? dataAgreement["dataAttributes"].length
+                      : 0;
+                  const checked =
+                    dataAgreement["consentRecords"] === null
+                      ? false
+                      : dataAgreement["consentRecords"]["optIn"];
 
-    return !data ? null : (
-      <Collapse
-        className="collapse-usage"
-        bordered={false}
-        defaultActiveKey={openKey.toString()}
-        activeKey={activeKeys}
-        onChange={this.onCollapseChange}
-        expandIcon={({ isActive }) => (
-          <Icon
-            style={{ position: "absolute", top: "22px" }}
-            type="caret-right"
-            rotate={isActive ? 90 : 0}
-          />
-        )}
-      >
-        {!data
-          ? null
-          : data.map((dataAgreement, i) => {
-              const consented =
-                dataAgreement["consentRecords"] === null
-                  ? 0
-                  : dataAgreement["consentRecords"]["optIn"]
-                  ? dataAgreement["dataAttributes"].length
-                  : 0;
-              const checked =
-                dataAgreement["consentRecords"] === null
-                  ? false
-                  : dataAgreement["consentRecords"]["optIn"];
+                  const disableDataAttributes =
+                    dataAgreement["methodOfUse"] !== "data_using_service" ||
+                    (!dataAgreement["dataSources"] ||
+                      dataAgreement["dataSources"] === undefined ||
+                      dataAgreement["dataSources"].length === 0);
 
-              const isActive = activeKeys.includes(i.toString());
-              return (
-                <Panel
-                  header={
-                    <Spin
+                  return (
+                    <Panel
                       key={i}
-                      spinning={loadingUiStore[dataAgreement["id"]]}
-                    >
-                      <PanelHeaderWithNamespace
-                        elemRef={this.elemRefs[dataAgreement["id"]]}
-                        policyURL={dataAgreement["policy"]["url"]}
-                        disabled={dataAgreement["isDisabled"]}
-                        consented={consented}
-                        total={dataAgreement["dataAttributes"].length}
-                        checked={checked}
-                        onChange={(value, e) =>
-                          this.handleSwitch(dataAgreement, value, e)
-                        }
-                        text={`${dataAgreement["purpose"]}`}
-                        description={`${dataAgreement["purposeDescription"]}`}
-                        showDescription={isActive}
-                        dataAgreement={dataAgreement}
-                        lawfulBasisOfProcessing={`${
-                          dataAgreement["lawfulBasis"]
-                        }`}
-                        jurisdiction={dataAgreement["policy"]["jurisdiction"]}
-                        thirdPartyDisclosure={
-                          dataAgreement["policy"]["thirdPartyDataSharing"]
-                            ? "True"
-                            : "False"
-                        }
-                        industryScope={
-                          dataAgreement["policy"]["industrySector"]
-                        }
-                        geographicRestriction={
-                          dataAgreement["policy"]["geographicRestriction"]
-                        }
-                        retentionPeriod={`${Math.floor(
-                          dataAgreement["policy"]["dataRetentionPeriodDays"] /
-                            365
-                        )} years`}
-                        storageLocation={
-                          dataAgreement["policy"]["storageLocation"]
-                        }
-                        methodOfUse={`${dataAgreement["methodOfUse"]}`}
-                      />
-                    </Spin>
-                  }
-                  key={i}
-                  className={`panel-usage panel-usage-${dataAgreement["id"]}`}
-                >
-                  {!dataAgreement["dataAttributes"]
-                    ? null
-                    : dataAgreement["dataAttributes"].map(
-                        (dataAttribute, i) => (
-                          <SubCollapse
+                      header={
+                        <Spin
+                          key={i}
+                          spinning={loadingUiStore[dataAgreement["id"]]}
+                        >
+                          <PanelHeaderWithNamespace
+                            elemRef={this.elemRefs[dataAgreement["id"]]}
+                            policyURL={dataAgreement["policy"]["url"]}
+                            disabled={dataAgreement["isDisabled"]}
+                            consented={consented}
+                            total={dataAgreement["dataAttributes"].length}
+                            checked={checked}
+                            onChange={(value, e) =>
+                              this.handleSwitch(dataAgreement, value, e)
+                            }
+                            text={`${dataAgreement["purpose"]}`}
+                            description={`${
+                              dataAgreement["purposeDescription"]
+                            }`}
+                            disableDataAttributes={disableDataAttributes}
                             key={i}
-                            lawfulUsage={dataAgreement["isDisabled"]}
-                            title={dataAttribute["name"]}
-                            consented={checked}
+                            setOpenViewDataSources={(e) =>
+                              this.openDataSourcesModal(dataAgreement, e)
+                            }
+                            onOpenDataAgreementModal={(e) =>
+                              this.openDataAgreementModal(dataAgreement, e)
+                            }
                           />
-                        )
-                      )}
-                </Panel>
-              );
-            })}
-      </Collapse>
+                        </Spin>
+                      }
+                      className={`panel-usage panel-usage-${
+                        dataAgreement["id"]
+                      }`}
+                    >
+                      {!dataAgreement["dataAttributes"]
+                        ? null
+                        : dataAgreement["dataAttributes"].map(
+                            (dataAttribute, i) => (
+                              <SubCollapse
+                                lawfulUsage={dataAgreement["isDisabled"]}
+                                title={dataAttribute["name"]}
+                                consented={checked}
+                              />
+                            )
+                          )}
+                    </Panel>
+                  );
+                })}
+          </Collapse>
+        )}
+
+        {/* View Data Sources Modal */}
+        {openDataSources && (
+          <ModalComponentWithNamespace
+            open={openDataSources}
+            setOpen={this.setOpenViewDataSources}
+            header={t("dataAgreements.dataSources")}
+          >
+            <DataSourcesDetailsWithNamespace
+              dataAgreement={selectedDataAgreement}
+            />
+          </ModalComponentWithNamespace>
+        )}
+
+        {/* View Data Agreement Modal */}
+        {openDataAgreement && (
+          <ModalComponentWithNamespace
+            open={openDataAgreement}
+            setOpen={(open) => this.setState({ openDataAgreement: open })}
+            header={t("dataAgreements.daataAgreement")}
+          >
+            <DataAgreementDetailsWithNamespace
+              purpose={selectedDataAgreement && selectedDataAgreement.purpose}
+              purposeDescription={
+                selectedDataAgreement &&
+                selectedDataAgreement.purposeDescription
+              }
+              lawfulBasisOfProcessing={
+                selectedDataAgreement && selectedDataAgreement.lawfulBasis
+              }
+              policyURL={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.url
+              }
+              jurisdiction={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.jurisdiction
+              }
+              thirdPartyDisclosure={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.thirdPartyDataSharing
+                  ? "True"
+                  : "False"
+              }
+              industryScope={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.industrySector
+              }
+              geographicRestriction={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.geographicRestriction
+              }
+              retentionPeriod={
+                selectedDataAgreement && selectedDataAgreement.policy
+                  ? `${Math.floor(
+                      selectedDataAgreement.policy.dataRetentionPeriodDays / 365
+                    )} years`
+                  : ""
+              }
+              storageLocation={
+                selectedDataAgreement &&
+                selectedDataAgreement.policy &&
+                selectedDataAgreement.policy.storageLocation
+              }
+            />
+          </ModalComponentWithNamespace>
+        )}
+      </>
     );
   }
 }
